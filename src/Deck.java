39ecada1ac3d48da02e72726a573ef61254efade
owenlessel <owenlessel@gmail.com>
2022-02-01 09:50:34 -0600

    done with 1-13 Start On 14

--- src/Deck.java
+++ src/Deck.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 public class Deck {
     // PROPERTIES
     private Card[] cards;
     // CONSTRUCTORS
     public Deck() {
         this.cards = new Card[52];
+        this.makeDeck();
     }
     public Deck(int suits,int cardsPerSuit,boolean aceHighLow){
 
@@ -21,9 +23,30 @@ public class Deck {
     // METHODS
     public void shuffleCards() {
         // Shuffle this.cards in a random order
+        Random rand = new Random();
     }
     private void makeDeck(){
-        
+        int counter=0;
+        for(int i=2;i<=14;i++){
+            Card temp = new Card("red", 'h', 'h', i, true);
+            this.cards[counter]= temp;
+            counter++;
+        }
+        for(int i=2;i<=14;i++){
+            Card temp = new Card("red", 'd', 'd', i, true);
+            this.cards[counter]= temp;
+            counter++;
+        }
+        for(int i=2;i<=14;i++){
+            Card temp = new Card("black", 'c', 'c', i, true);
+            this.cards[counter]= temp;
+            counter++;
+        }
+        for(int i=2;i<=14;i++){
+            Card temp = new Card("black", 's', 's', i, true);
+            this.cards[counter]= temp;
+            counter++;
+        }
         
     }
 }
