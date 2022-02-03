aa7fbed4d2ec6c1d42840f5334d5a6e12ab4eab9
owenlessel <owenlessel@gmail.com>
2022-02-03 10:02:22 -0600

    progress on 14 start working on client

--- src/CardGameClient.java
+++ src/CardGameClient.java
@@ -1,9 +1,7 @@
 public class CardGameClient {
     public static void main(String[] args) throws Exception {
-        Card C1 = new Card("red", 'K', 'L', 13, true);
-        Card C2 = new Card("red", 'K', 'L', 13, true);
-
-        System.out.println(C1.cardEqual(C2));
+        Deck test1 = new Deck();
+        System.out.println(test1);
 
     }
 }
